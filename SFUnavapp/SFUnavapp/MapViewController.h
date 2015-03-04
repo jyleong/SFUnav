05917781bf8ba08b38918d95b6133de9bba74671
Arjun Rathee <arathee@sfu.ca>
2015-03-03 17:14:51 -0800

    map view added. Loads campus map and displays alert view on click of building -Map still doesn’t resize to fit screen

--- SFUnavapp/SFUnavapp/MapViewController.h
+++ SFUnavapp/SFUnavapp/MapViewController.h
@@ -2,12 +2,15 @@
 //  MapViewController.h
 //  SFUnavapp
 //
-//  Created by James Leong on 2015-02-12.
-//  Copyright (c) 2015 James Leong. All rights reserved.
+//  Created by Arjun Rathee on 2015-03-03.
+//  Copyright (c) 2015 Team NoMacs. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import "MTImageMapView.h"
 
-@interface MapViewController : UIViewController
+@interface MapViewController : UIViewController <MTImageMapDelegate>
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (nonatomic, strong) NSArray *BuildingNames;
 
 @end
--- SFUnavapp/SFUnavapp/MapViewController.m
+++ SFUnavapp/SFUnavapp/MapViewController.m
@@ -2,8 +2,8 @@
 //  MapViewController.m
 //  SFUnavapp
 //
-//  Created by James Leong on 2015-02-12.
-//  Copyright (c) 2015 James Leong. All rights reserved.
+//  Created by Arjun Rathee on 2015-03-03.
+//  Copyright (c) 2015 Team NoMacs. All rights reserved.
 //
 
 #import "MapViewController.h"
@@ -26,8 +26,35 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.navigationItem.title = @"Map";
     // Do any additional setup after loading the view.
+    
+    
+    //Change name to Campus_Map.png for labels and legend in the image, map-Campus-01.png for no lables and no legend
+    MTImageMapView *viewImageMap =[[MTImageMapView alloc] initWithImage: [UIImage imageNamed:@"map-Campus-01.png"]];
+    
+    [viewImageMap setDelegate:self];
+    
+    [self.scrollView addSubview:viewImageMap];
+    //sets scrollview size to be the same as imagemap size to allow scrolling
+    self.scrollView.contentSize = viewImageMap.frame.size;
+    
+    
+    
+    self.BuildingNames = [NSArray arrayWithContentsOfFile: [[NSBundle mainBundle] pathForResource:@"Buildings_name" ofType:@"plist"]];
+    
+    NSArray *arrBuildings =[NSArray arrayWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"Buildings_coord"ofType:@"plist"]];
+    NSLog(@"arr size %lu",(unsigned long)[arrBuildings count]);
+    [viewImageMap setMapping:arrBuildings doneBlock:^(MTImageMapView *imageMapView) {NSLog(@"Areas are all mapped"); }];
+    
+    
+    
+
+}
+
+-(void)imageMapView:(MTImageMapView *)inImageMapView
+   didSelectMapArea:(NSUInteger)inIndexSelected
+{
+    [[[UIAlertView alloc] initWithTitle:@"*** Building Name ***" message:[_BuildingNames objectAtIndex:inIndexSelected] delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil] show];
 }
 
 - (void)didReceiveMemoryWarning
