81faefa874f26255e3ec6b0a2677ca6b71704a4f
James Leong <jyleong@sfu.ca>
2015-03-17 11:36:24 -0700

    added google markers( their pins) , appear on long touch and clear button

--- SFUnavapp/SFUnavapp/OnlineMapViewController.m
+++ SFUnavapp/SFUnavapp/OnlineMapViewController.m
@@ -10,6 +10,7 @@
 #import <GoogleMaps/GoogleMaps.h>
 
 @interface OnlineMapViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *clrBtn;
 
 
 @end
@@ -30,14 +31,19 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.navigationController.navigationBar.topItem.title = @"";
     // Create a GMSCameraPosition that tells the map to display the
     // coordinate at SFU at zoom level 10.
     GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:49.278094
                                                             longitude:-122.919883
                                                                  zoom:15];
     _sfumapView = [GMSMapView mapWithFrame:CGRectZero camera:camera];
+    [self.view insertSubview:_sfumapView atIndex:0];
     _sfumapView.myLocationEnabled = YES;
     self.view = _sfumapView;
+    _sfumapView.delegate = self;
+    [self.view addSubview:_clrBtn];
     
     // Creates a marker in the center of the map.
     GMSMarker *marker = [[GMSMarker alloc] init];
@@ -47,10 +53,26 @@
     marker.map = _sfumapView;
 }
 
+-(void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    self.navigationItem.title=@"Google Maps";
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)mapView:(GMSMapView *)mapView didLongPressAtCoordinate:(CLLocationCoordinate2D)coordinate {
+    GMSMarker *usertapMarker = [[GMSMarker alloc] init];
+    usertapMarker.appearAnimation = kGMSMarkerAnimationPop;
+    usertapMarker.position = coordinate;
+    usertapMarker.title = [NSString stringWithFormat:@"%f, %f", coordinate.latitude, coordinate.longitude];
+    usertapMarker.map = _sfumapView;
+}
+- (IBAction)clearMark:(id)sender {
+    [_sfumapView clear];
 }
 
 
