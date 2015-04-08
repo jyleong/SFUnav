978101d32db1b057b6c9a2723849b4befb49cfe9
Serena Chan <serena.sy.c@gmail.com>
2015-04-08 00:25:13 -0700

    library: fixed reload problem

--- SFUnavapp/SFUnavapp/LibraryHoursTableViewCell.h
+++ SFUnavapp/SFUnavapp/LibraryHoursTableViewCell.h
@@ -2,17 +2,15 @@
 //  LibraryHoursTableViewCell.h
 //  SFUnavapp
 //
-//  Created by Serena Chan on 2015-03-29.
+//  Created by Serena Chan on 2015-04-07.
 //  Copyright (c) 2015 Team NoMacs. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface LibraryHoursTableViewCell : UITableViewCell
-
-@property (strong, nonatomic) IBOutlet UILabel *libraryName;
-@property (strong, nonatomic) IBOutlet UILabel *libraryStatus;
-@property (strong, nonatomic) IBOutlet UILabel *openTime;
-@property (strong, nonatomic) IBOutlet UILabel *closeTime;
-
+@property (strong,nonatomic) UILabel *libraryName;
+@property (strong,nonatomic) UILabel *libraryStatus;
+@property (strong,nonatomic) UILabel *openTime;
+@property (strong,nonatomic) UILabel *closeTime;
 @end
